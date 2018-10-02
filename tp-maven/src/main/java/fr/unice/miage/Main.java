2c3432f75b5685303ad4f4a5626320937804b3d4
Romain Lembo <romainlembo06@gmail.com>
2018-10-02 11:04:17 +0200

    responses for 'Ca marche' 8 and compile project

---
+++ tp-maven/src/main/java/fr/unice/miage/Main.java
@@ -0,0 +1,13 @@
+package fr.unice.miage;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello MIAGE !" );
+    }
+}
